8adf14c6f984939d9d76a191cc51223a70ace630
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2024-12-10 17:43:57 -0500

    add files for 0.1.0-rc1

---
+++ scripts/update-version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check arguments
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <new_version>"
+  exit 1
+fi
+
+NEW_VERSION=$1
+# REPO_FILE=$2
+# CONFIG_FILE=$3
+
+REPO_FILE=repository.yaml
+CONFIG_FILE=microgreen-server/config.yaml
+
+# Update repository.yaml
+yq -i ".version = \"$NEW_VERSION\"" "$REPO_FILE"
+yq -i ".\"add-ons\"[0].version = \"$NEW_VERSION\"" "$REPO_FILE"
+
+# Update config.yaml
+yq -i ".version = \"$NEW_VERSION\"" "$CONFIG_FILE"
+yq -i ".image = (.image | split(\":\")[0] + \":$NEW_VERSION\")" "$CONFIG_FILE"
+
+echo "Updated version to $1"
